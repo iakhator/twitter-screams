54f8d1c5f51b1afc8b09861a208a4e66832bbfdb
Akhator Itua <iakhator@gmail.com>
2019-11-09 15:35:36 +0100

    delete scream

--- functions/handlers/scream.js
+++ functions/handlers/scream.js
@@ -193,3 +193,25 @@ exports.unlikeScream = (req, res) => {
       res.status(500).json({ error: err.code });
     });
 };
+
+exports.deleteScream = (req, res) => {
+  const document = db.doc(`/screams/${req.params.screamId}`);
+  document
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Scream not found" });
+      }
+      if (doc.data().handle !== req.user.handle) {
+        return res.status(403).json({ error: "unauthorized" });
+      } else {
+        document.delete().then(() => {
+          return res.json({ message: "Scream deleted successfully" });
+        });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
